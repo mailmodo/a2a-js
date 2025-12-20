800128cac0ba7667a72099910f473f6fe9a7ab33
Aryan Raj <aryaraj132@gmail.com>
2025-12-20 07:59:05 +0530

    fix: lint issues

--- src/samples/agents/movie-agent/genkit.ts
+++ src/samples/agents/movie-agent/genkit.ts
@@ -1,12 +1,9 @@
 import { googleAI } from '@genkit-ai/googleai';
 import { genkit } from 'genkit';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-
 export const ai = genkit({
   plugins: [googleAI()],
   model: googleAI.model('gemini-2.0-flash'),
-  promptDir: dirname(fileURLToPath(import.meta.url)),
+  promptDir: __dirname,
 });
 
 export { z } from 'genkit';
--- tsup.config.ts
+++ tsup.config.ts
@@ -12,6 +12,6 @@ export default defineConfig({
   outExtension({ format }) {
     return {
       js: `.${format === 'cjs' ? 'js' : 'mjs'}`,
-    }
+    };
   },
 });
