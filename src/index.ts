9ef1f79f50e0c8ddfa9144afd6362c3174356db6
Aryan Raj <aryaraj132@gmail.com>
2026-01-05 14:17:44 +0530

    fix: separate out exports and and types

--- src/index.ts
+++ src/index.ts
@@ -6,22 +6,6 @@
  */
 
 export * from './types.js';
-export * from './server/index.js';
-export * from './client/index.js';
-export * from './server/express/index.js';
-
-export { A2AError } from './server/index.js';
-export {
-  AuthenticatedExtendedCardNotConfiguredError,
-  ContentTypeNotSupportedError,
-  InvalidAgentResponseError,
-  PushNotificationNotSupportedError,
-  TaskNotCancelableError,
-  TaskNotFoundError,
-  UnsupportedOperationError,
-} from './client/index.js';
 export type { A2AResponse } from './a2a_response.js';
 export { AGENT_CARD_PATH, HTTP_EXTENSION_HEADER } from './constants.js';
 export { Extensions, type ExtensionURI } from './extensions.js';
-
-export * from './server/utils.js'; // Added this line
